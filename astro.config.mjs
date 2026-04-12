180db79aa53bab94a78d2807dafe9cc5dd5949cd
Romain <niamor.lth@gmail.com>
2026-04-12 19:09:51 +0200

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kulinia-marketing.romain-lthl.workers.dev",
+  site: "https://www.kulinia.com",
   vite: {
     plugins: [tailwindcss()],
   },
